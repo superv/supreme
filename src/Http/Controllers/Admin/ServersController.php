fd1f92eeb5076017f2c7079c733a29ace0f987d5
Ali Selcuk <maselcuk@gmail.com>
2017-06-01 10:54:45 +0300

    twig, sockets, etc

--- src/Http/Controllers/Admin/ServersController.php
+++ src/Http/Controllers/Admin/ServersController.php
@@ -1,9 +1,16 @@
 <?php namespace SuperV\Modules\Supreme\Http\Controllers\Admin;
 
+use SuperV\Platform\Domains\Asset\Asset;
+
 class ServersController
 {
-    public function index()
+    public function index(Asset $asset)
     {
-        return view()->make('superv.modules.supreme::home');
+        $asset->add('theme.css', 'droplets/superv/themes/spa/resources/scss/toastr.scss');
+//        $asset->add('theme.css', 'droplets/superv/themes/spa/resources/scss/theme/theme.scss');
+
+        $data['asset'] = $asset;
+        $data['name'] ='alis';
+        return view()->make('superv.modules.supreme::home', $data);
     }
 }
